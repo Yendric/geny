5481ef025995d5b920a2da46731692dbd0da2e5c
Yendric <yendricvanroey@gmail.com>
2024-01-10 16:52:48 +0100

    Remove website content

--- content/posts/blog.md
+++
@@ -1,6 +0,0 @@
----
-title: Yendrics blogposts
-description: Heeel af en toe schrijf ik eens een blogpost of tutorial. Die vind je hier.
----
-
-# Blog
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./templates/**/*", "./content/**/*"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
